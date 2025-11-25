3538970068b86c68c8bb1ebbeccf4133cfdd924b
Hashim Khushal Khan <hashimatd02@gmail.com>
2025-11-25 12:26:29 +0500

    copying node to util/node

--- include/utils/Nodes.h
+++ include/utils/Nodes.h
@@ -16,7 +16,7 @@ class Student;
 class Class;
 class Department;
 class School;
-class Faculty; // idk if needed or not
+class Faculty; // yes needed; 25/11/25 11:58 AM
 
 // Medical entities
 class Doctor;
@@ -47,5 +47,110 @@ class Library;
 
 // ==================== NODE STRUCTURES ====================
 // the rest of the node structures would go here...
+
+class Student
+{
+public:
+    std::string studentID;
+    std::string name;
+    int age;
+
+    Student* next;
+
+    Student(string id = "", string nam = "", int ag = 0)
+    {
+        studentID = id;
+        name = nam;
+        age = ag;
+        next = nullptr;
+    }
+
+};
+
+
+class Class
+{
+public:
+    std::string classID;
+    std::string className;
+
+
+    Student* studentHead;
+
+    Class* next;
+
+    Class()
+    {
+        studentHead = nullptr;
+        next = nullptr;
+    }
+};
+
+class Department
+{
+public:
+    std::string deptID;
+    std::string deptName;
+
+
+    Class* classHead;
+
+    Department* next;
+
+    Department()
+    {
+        classHead = nullptr;
+        next = nullptr;
+    }
+};
+
+class Faculty
+{
+public:
+    string FacultyID;
+    string name;
+    string specialization;
+
+    Faculty* next;
+
+    Faculty(string id = "", string nam = "", string spec = "")
+    {
+        FacultyID = id;
+        name = nam;
+        specialization = spec;
+        next = nullptr;
+    }
+};
+
+class School
+{
+public:
+    string schoolID;
+    string schoolName;
+    string sector;
+    float rating;
+    int campusNo;
+
+    string* subjects;
+    int subjectCount;
+
+    Department* deptHead;
+
+    Faculty* FacultyHead;
+
+    School* next; // n ary trees
+
+    School()
+    {
+        subjects = nullptr;
+        subjectCount = 0;
+
+        deptHead = nullptr;
+        FacultyHead = nullptr;
+
+        next = nullptr;
+    }
+};
+
 
 #endif
