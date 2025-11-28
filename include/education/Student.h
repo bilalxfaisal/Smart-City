9b3424e45b86fef732200ee287929f84c8de6653
Hadiah-Batool <hdbatool779@gmail.com>
2025-11-28 08:03:53 +0500

    StudentnDept

--- include/education/Student.h
+++ include/education/Student.h
@@ -6,7 +6,7 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
-
+//Linked List of students.
 class Student
 {
 public:
@@ -29,6 +29,56 @@ public:
 };
 
 
+
+
+string Student::getStudentID() { return studentID; }
+string Student::getStudentName() { return name; }
+void Student::printStudentDetails()
+{
+	cout << "ID: " << studentID << " | ";
+	cout << "Name: " << name << " | ";
+	cout << "Age: " << age << endl;
+}
+void insertStudent(Student* head, string id, string name, int age) 
+{
+    Student* newStudent = new Student(id, name, age);
+    newStudent->next = head;
+    head = newStudent;
+
+}
+void deleteStudent(Student** head_ref, string key) 
+{
+    //DELETE HEAD
+    Student* temp = *head_ref, * prev = nullptr;
+    if (temp != nullptr && temp->studentID == key) 
+    {
+        *head_ref = temp->next; 
+        delete temp;            
+        return;
+    }
+	//DELETE OTHER THAN HEAD
+    while (temp != nullptr && temp->studentID != key) 
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+	// If key was not present in linked list
+    if (temp == nullptr)
+    {
+        return;
+    }
+    prev->next = temp->next;
+    delete temp; 
+}
+void printStudents(Student* node) 
+{
+    while (node != nullptr) 
+    {
+        node->printStudentDetails();
+        node = node->next;
+    }
+}
+//Linked List of Faculty
 class Faculty
 {
 public:
@@ -44,16 +94,46 @@ public:
         specialization = spec;
         next = nullptr;
     }
+    void InsertFaculty(Faculty** head_ref, string id, string name, string spec) 
+    {
+        Faculty* newFaculty = new Faculty(id, name, spec);
+        newFaculty->next = *head_ref;
+        *head_ref = newFaculty;
+	}
+    void deleteFaculty(Faculty** head_ref, string key) 
+    {
+        Faculty* temp = *head_ref, * prev = nullptr;
+        if (temp != nullptr && temp->FacultyID == key) 
+        {
+            *head_ref = temp->next; 
+            delete temp;            
+            return;
+        }
+        while (temp != nullptr && temp->FacultyID != key) 
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+        if (temp == nullptr)
+        {
+            return;
+        }
+        prev->next = temp->next;
+        delete temp; 
+	}
+    void printFaculties(Faculty* node) 
+    {
+        while (node != nullptr) 
+        {
+            cout << "Faculty ID: " << node->FacultyID << " | ";
+            cout << "Name: " << node->name << " | ";
+            cout << "Specialization: " << node->specialization << endl;
+            node = node->next;
+        }
+	}
 };
 
-string Student::getStudentID() { return studentID; }
-string Student::getStudentName() { return name; }
-void Student::printStudentDetails()
-{
-	cout << "ID: " << studentID << " | ";
-	cout << "Name: " << name << " | ";
-	cout << "Age: " << age << endl;
-}
+
 
 
 #endif
